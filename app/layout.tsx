47f6df550c54bff2a3ab27da2113934bf2ec405c
bamba <bba@wheretech.it>
2024-06-25 11:48:22 +0200

    Setup tailwind and daisy ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
+
+import './global.css';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme='light'>
       <body>{children}</body>
     </html>
   );
